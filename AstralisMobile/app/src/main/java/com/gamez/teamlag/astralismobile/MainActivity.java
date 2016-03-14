48b580410a47065a31b5b7af30296f15f903c86d
angpulangpluma <mariennelopez30@gmail.com>
2016-03-14 09:53:02 +0800

    astralis mobile - formatting and linking
    - main menu is properly formatted
    - 'settings' option redirects

--- AstralisMobile/app/src/main/java/com/gamez/teamlag/astralismobile/MainActivity.java
+++ AstralisMobile/app/src/main/java/com/gamez/teamlag/astralismobile/MainActivity.java
@@ -1,13 +1,23 @@
 package com.gamez.teamlag.astralismobile;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
